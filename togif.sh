fc2d82fc5e0dde0dd4bdfdab22904bfc576e5443
joemartinson <martinsonjoe2@gmail.com>
2020-10-30 23:47:01 -0700

    POSIX compliant shell implementation

--- togif.sh
+++ togif.sh
@@ -1,8 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 
-if [ "$1" == "" ]; then
-    echo "[Error]: No file specified"
-    echo "Usage: ./togif.sh <file>"
-    exit 1
-fi
-ffmpeg -i $1 -f gif "$1.gif"
+["$1" == ""] && echo "[Error]: No file specified " && echo "Usage: ./togif.sh <file>" && exit 1 && ffmpeg -i $1 -f gif "$1.gif"
