66eb099f3076cac4fb72609da0efa9a646a68f4f
joemartinson <martinsonjoe2@gmail.com>
2020-10-30 23:57:50 -0700

    improved slightly

--- togif.sh
+++ togif.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-["$1" == ""] && echo "[Error]: No file specified " && echo "Usage: ./togif.sh <file>" && exit 1 && ffmpeg -i $1 -f gif "$1.gif"
+[ -z "$1" ] && echo "[Error]: No file specified " && echo "Usage: ./togif.sh <file>" && exit 1 ||  ffmpeg -i $1 -f gif "$1.gif"
